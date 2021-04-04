9bad697d0f3def773661910971b01795ee295228
Swetha Lakshme S <swethalakshme02@gmail.com>
2021-04-04 18:41:20 +0530

    Upload file page

--- manufacturerAuth.js
+++ manufacturerAuth.js
@@ -39,7 +39,7 @@ signinForm.addEventListener('submit',(e) => {
         console.log(cred.user); 
         window.alert("Logged In successfully!!");
         const modal = document.querySelector('#sign-in');  
-        window.location.href="index.html";
+        window.location.href="upload.html";
     })
     .catch((error) => {
         var errorCode = error.code; 
