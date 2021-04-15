f5ca0bd69dea6234d00f308368737f2b12cd4b49
Dharshini1810 <70425084+Dharshini1810@users.noreply.github.com>
2021-04-15 13:50:35 +0530

    linking pages

--- supplierAuth.js
+++ supplierAuth.js
@@ -12,7 +12,7 @@ signupForm.addEventListener('submit',(e) => {
         console.log(cred.user); 
         window.alert("Account Created Sucessfully!!");
         const modal = document.querySelector('#sign-up'); 
-        window.location.href="imgupload.html";
+        window.location.href="supplierReg.html";
 
     }) 
     .catch((error) => {
