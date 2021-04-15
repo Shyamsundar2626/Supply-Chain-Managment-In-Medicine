957411df5d761ea124b4c36a5d0ada3e0253c27d
Dharshini1810 <70425084+Dharshini1810@users.noreply.github.com>
2021-04-15 11:21:13 +0530

    supplier and consumer img and qr code page

--- supplierAuth.js
+++ supplierAuth.js
@@ -12,7 +12,7 @@ signupForm.addEventListener('submit',(e) => {
         console.log(cred.user); 
         window.alert("Account Created Sucessfully!!");
         const modal = document.querySelector('#sign-up'); 
-        window.location.href="supplierReg.html";
+        window.location.href="imgupload.html";
 
     }) 
     .catch((error) => {
@@ -39,7 +39,7 @@ signinForm.addEventListener('submit',(e) => {
         console.log(cred.user); 
         window.alert("Logged In successfully!!");
         const modal = document.querySelector('#sign-in');  
-        window.location.href="index.html";
+        window.location.href="imgupload.html";
     })
     .catch((error) => {
         var errorCode = error.code; 
