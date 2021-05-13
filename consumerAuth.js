32a2597718fe9892e9e1bb08d9c5946fc4e0e22b
Thoshinny B <sec19cs051@sairamtap.edu.in>
2021-05-13 13:07:50 +0530

    backend added fully

--- consumerAuth.js
+++ consumerAuth.js
@@ -1,32 +1,32 @@
 const signupForm = document.querySelector('#sign-up-form');
-signupForm.addEventListener('submit',(e) => {
+signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    //get user info 
-    const email = signupForm['signup-email'].value; 
-    const password = signupForm['signup-password'].value;
-    
-    //sign up the user
-    auth.createUserWithEmailAndPassword(email,password)
-    .then(cred =>{
-        console.log(cred.user); 
-        window.alert("Account Created Sucessfully!!");
-        const modal = document.querySelector('#sign-up'); 
-        window.location.href="qrcodeupload.html";
 
-    }) 
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        window.alert("Email id Already Exist"); 
-        signupForm.reset();
-    
-    })
+    //get user info 
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(cred => {
+            console.log(cred.user);
+            window.alert("Account Created Sucessfully!!");
+            const modal = document.querySelector('#sign-up');
+            window.location.href = "qrcodeupload.html";
+
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            window.alert("Email id Already Exist");
+            signupForm.reset();
+
+        })
 });
 
 
 const signinForm = document.querySelector('#signin-form');
-signinForm.addEventListener('submit',(e) => {
+signinForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     //get user info 
@@ -34,17 +34,17 @@ signinForm.addEventListener('submit',(e) => {
     const password = signinForm['signin-password'].value;
 
     //sign up the user 
-    auth.signInWithEmailAndPassword(email,password)
-    .then(cred =>{
-        console.log(cred.user); 
-        window.alert("Logged In successfully!!");
-        const modal = document.querySelector('#sign-in');  
-        window.location.href="qrcodereader.html";
-    })
-    .catch((error) => {
-        var errorCode = error.code; 
-        var errorMessage = error.message; 
-        window.alert("Invalid Email id");
-        signinForm.reset();
-    })
+    auth.signInWithEmailAndPassword(email, password)
+        .then(cred => {
+            console.log(cred.user);
+            window.alert("Logged In successfully!!");
+            const modal = document.querySelector('#sign-in');
+            window.location.href = "qrcodeupload.html";
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            window.alert("Invalid Email id");
+            signinForm.reset();
+        })
 });
