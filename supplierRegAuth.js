31f0231147109816a30553b9b71f2e4915f5cf7b
Thoshinny B <sec19cs051@sairamtap.edu.in>
2021-05-16 16:55:49 +0530

    project fully working

--- supplierRegAuth.js
+++ supplierRegAuth.js
@@ -6,7 +6,7 @@ var firebaseConfig = {
     storageBucket: "supplier-regform-1.appspot.com",
     messagingSenderId: "64267486720",
     appId: "1:64267486720:web:5ffb225e70e5410e59fae5"
-  };
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -17,53 +17,48 @@ var messagesRef = firebase.database().ref('messages');
 document.getElementById('supplierform').addEventListener('submit', submitForm);
 
 //Submit Form
-function submitForm(e){
+function submitForm(e) {
     e.preventDefault();
 
-//Get Values
-var fname = getInputVal('fname');
-var lname = getInputVal('lname');
-var emailid = getInputVal('emailid');
-var mname = getInputVal('mname');
-var address = getInputVal('address');
-var postalcode = getInputVal('postalcode');
-var medno = getInputVal('medno');
-var phno = getInputVal('phno');
-var govlic = getInputVal('govlic');    
+    //Get Values
+    var fname = getInputVal('fname');
+    var lname = getInputVal('lname');
+    var emailid = getInputVal('emailid');
+    var mname = getInputVal('mname');
+    var address = getInputVal('address');
+    var postalcode = getInputVal('postalcode');
+    var medno = getInputVal('medno');
+    var phno = getInputVal('phno');
+    var govlic = getInputVal('govlic');
 
-//Save Message 
-saveMessage(fname, lname,emailid, mname, address, postalcode, medno, phno, govlic); 
+    //Save Message 
+    saveMessage(fname, lname, emailid, mname, address, postalcode, medno, phno, govlic);
 
-//Alert Message
-window.alert('Your messages has been sent!'); 
-window.location.href="qrcodereader.html";
+    //Alert Message
+    window.alert('Your messages has been sent!');
+    window.location.href = "index.html";
 
-// Clear form
-document.getElementById('supplierform').reset();
+    // Clear form
+    document.getElementById('supplierform').reset();
 }
 
 // Function to get get form values
-function getInputVal(id){
+function getInputVal(id) {
     return document.getElementById(id).value;
 }
 
 // Save message to firebase
-function saveMessage(fname, lname, emailid, name, address, postalcode, medno, phno, govlic){
+function saveMessage(fname, lname, emailid, name, address, postalcode, medno, phno, govlic) {
     var newMessageRef = messagesRef.push();
     newMessageRef.set({
-      fname: fname ,
-      lname: lname,
-      emailid: emailid,
-      name: name, 
-      address: address,
-      postalcode: postalcode,
-      medno: medno,
-      phno: phno,
-      govlic: govlic
+        fname: fname,
+        lname: lname,
+        emailid: emailid,
+        name: name,
+        address: address,
+        postalcode: postalcode,
+        medno: medno,
+        phno: phno,
+        govlic: govlic
     });
-  }
-
-
-
-
-
+}
